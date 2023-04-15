8bb38880829244c06610e3d871b6925c6ff99936
hansololiviakim <dev.hansolkim@gmail.com>
2023-04-16 01:08:34 +0900

    Note: useState 실습

--- beginner/1_week-1/src/App.jsx
+++ beginner/1_week-1/src/App.jsx
@@ -1,45 +1,54 @@
 import React, { useState } from 'react'
 
+// ! 실습
+// 아이디에 및 비밀번호에 값을 입력하고 [로그인] 버튼을 누르면 alert로 고객이 입력한 값 알려주기
+// alert -> `고객님이 입력하신 아이디는 ${입력아이디}이며, 비밀번호는 ${입력비밀번호}입니다.`
+
+// ! 다음 사항을 준수할 것
+// 1. id와 pwd 필드의 값을 state로 관리하고, 변경이 일어날 때마다 setState로 동기화시킬 것
+// 2. alert를 띄운 이후에는 id와 pwd값 빈 값으로 초기화
+// 3. pwd는 보여지면 안됨
+
 function App() {
-  // ! State를 사용하는 목적 : UI를 변경하기 위해 (렌더링을 위해서) 별도로 상태를 관리하는 것!
-  // * useState : 괄호를 열고 닫으면 동작하는 hook (함수)
-  // 사용법 : useState('initial value');
-  // return : [state, setState] (state와 state를 다룰 수 있는 메서드) 배열
-  // 이 두 가지를 구조분해할당으로 다음과 같이 사용 가능하다.
-  // const [value, setValue] = useState('initial value');
+  const [id, setId] = useState('');
+  const [pwd, setPwd] = useState('');
 
-  // ex.
-  // const [count, setCount] = useState(0);
-  // const [todoList, setTodoList] = useState([]);
-
-  const [name, setName] = useState('김민지');
-  const [fruit, setFruit] = useState('');
+  const idChangeHandler = (event) => setId(event.target.value);
+  const pwdChangeHandler = (event) => setPwd(event.target.value);
+  const onClickHandler = () => {
+    alert(`고객님이 입력하신 아이디는 ${id}이며, 비밀번호는 ${pwd}입니다.`);
+    setId('');
+    setPwd('');
+  }
 
   return (
-    <>
-      <div>
-        {name}&nbsp;
-        <button onClick={function() {
-          setName('강해린');
-        }}>
-          클릭
-        </button>
-      </div>
-
-      <br />
-
-      <div>
-        {/* input 안에 입력되는 값을 fruit와 매칭시키려면 value와 onChange를 사용하면 된다 */}
-        {/* onChange에는 항상 evnet가 매개변수로 딸려온다 */}
-        과일 :&nbsp;
+    <div>
+      <label htmlFor="id">
+        아이디 :&nbsp;
         <input
-          value={fruit}
-          onChange={(event) => setFruit(event.target.value)}
+          type="text"
+          id="id"
+          value={id}
+          onChange={idChangeHandler}
         />
-        <br />
-        {fruit}
-      </div>
-    </>
+      </label>
+      <br />
+      <label htmlFor="pwd">
+        비밀번호 :&nbsp;
+        <input
+          type="password"
+          id="pwd"
+          value={pwd}
+          onChange={pwdChangeHandler}
+        />
+      </label>
+      <br />
+      <button
+        onClick={onClickHandler}
+      >
+        로그인
+      </button>
+    </div>
   )
 }
 
