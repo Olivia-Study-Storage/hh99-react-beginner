e877be2b2c2455ee5d65548ebf6f9838aea78f2b
hansololiviakim <dev.hansolkim@gmail.com>
2023-04-15 23:14:28 +0900

    Note: jsx 문법 실습

--- beginner/1_week-1/src/App.jsx
+++ beginner/1_week-1/src/App.jsx
@@ -1,21 +1,32 @@
-import React from 'react'
+import React from 'react';
 
-// 이와 같이 리액트는 컴포넌트끼리의 상호작용으로 개발된다.
+export default function App() {
+  const number = 1;
+  // 스타일을 변수로 선언하여 재사용 가능하도록 할 수 있다.
+  const pTagStyle = {
+    color: 'tomato',
+  };
 
-function Son() {
-  return <div>자식 컴포넌트</div>
+  return (
+    <div className="test-class">
+      {/* 주석을 사용하는 방법 */}
+
+      {/* 변수를 사용하지 않고 스타일을 지정하는 법 */}
+      {/* <p
+        style={{
+          color: 'salmon',
+        }}
+      > */}
+      
+      {/* 변수를 사용하여 스타일을 지정하는 법 */}
+      <p style={pTagStyle}> 안녕하세요 리액트입니다.</p>
+      <p>
+        {
+          number > 10
+            ? number + '은 10보다 크다'
+            : number + '은 10보다 작다'
+        }
+      </p>
+    </div>
+  )
 }
-
-function Mother() {
-  return <Son/>
-}
-
-function Grandfather() {
-  return <Mother/>
-}
-
-function App() {
-  return <Grandfather />
-}
-
-export default App
