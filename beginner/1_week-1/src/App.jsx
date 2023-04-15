fe182966ecfb264d1a01d9522276a4b8f6640c69
hansololiviakim <dev.hansolkim@gmail.com>
2023-04-15 23:13:28 +0900

    Note: 부모-자식 컴포넌트

--- beginner/1_week-1/src/App.jsx
+++ beginner/1_week-1/src/App.jsx
@@ -1,33 +1,16 @@
 import React from 'react';
+
+// 나중에 코드가 길어지면 일정한 부분들을 따로 빼서 컴포넌트화를 시킨다.
+// 왜? 반복되는 로직이 생길 경우 효율적인 코드 작성을 위해 필요하다.
+
+// 자식 컴포넌트 : 다른 컴포넌트 안에서 품어지는 컴포넌트
+function Child() {
+  return <div>나는 자식 컴포넌트입니다.</div>;
+}
+
+// 부모 컴포넌트 : 다른 컴포넌트를 품는 컴포넌트
 function App() {
-
-  // <---- 자바스크립트 영역 ---->
-  // function clickHandler() {
-  //   alert('클릭!');
-  // }
-
-  // 화살표 함수로 표현하기
-  const clickHandler = () => {
-    alert('클릭!');
-  }
-
-  return (
-  /* <---- HTML/JSX 영역  ---->*/
-    <div
-      // JSX 문법 안에서 태그에 스타일을 부여하기 위해서는
-      // key-value 페어로 되어있는 '객체'를 JS 문법으로 넣어주면 된다.
-      style={{
-        height: '100vh',
-        display: ' flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <p>이것은 내가 만든 App 컴포넌트 입니다.</p>
-      <button onClick={clickHandler}>클릭!</button>
-    </div>
-  );
+  return <Child />;
 }
 
 export default App;
