9c08361f1ae9617c6cda89ff1b02010b95113284
hansololiviakim <dev.hansolkim@gmail.com>
2023-04-16 16:58:09 +0900

    Note: 반복되는 컴포넌트 - 객체 삭제

--- beginner/1_week-1/src/App.jsx
+++ beginner/1_week-1/src/App.jsx
@@ -3,20 +3,24 @@ import './App.css';
 
 function App() {
   const [users, setUsers] = useState([]);
-
   const [name, setName] = useState('');
   const [age, setAge] = useState(0);
 
   const nameChangeHandler = (event) => setName(event.target.value);
   const ageChangeHandler = (event) => setAge(event.target.value);
-  const btnClickHandler = () => {
+  const addClickHandler = () => {
     const newUsers = {
       id: users.length + 1,
       age,
       name,
     };
     setUsers([...users, newUsers]);
-  }
+  };
+  const removeClickHandler = (id) => {
+    // 아래 한 줄만 적으면 필터링한 값을 useState에 반영하지 않았기 때문에 리렌더링 되지 않는다
+    const filterUsers = users.filter(user => user.id !== id);
+    setUsers(filterUsers);
+  };
 
   return (
     <div id="wrap">
@@ -42,7 +46,7 @@ function App() {
         </label>
         <br />
         <button
-          onClick={btnClickHandler}
+          onClick={addClickHandler}
         >추가</button>
       </div>
       <br />
@@ -51,6 +55,12 @@ function App() {
           return (
             <section key={item.id}>
               {item.age} - {item.name}
+              <button
+                // onClick={removeClickHandler(item.id)} 이렇게 바로 호출하면 안됨!
+                onClick={() => removeClickHandler(item.id)}
+              >
+                X
+              </button>
             </section>
           )
         })}
