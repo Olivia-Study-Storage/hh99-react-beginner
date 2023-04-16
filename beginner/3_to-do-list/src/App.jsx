48c5eff0984da569bb469773240276e7c32029e3
hansololiviakim <dev.hansolkim@gmail.com>
2023-04-17 03:52:53 +0900

    Rename: Form 컴포넌트 분리

--- beginner/3_to-do-list/src/App.jsx
+++ beginner/3_to-do-list/src/App.jsx
@@ -1,28 +1,10 @@
 import React, { useState } from 'react';
-import { nanoid } from 'nanoid';
 import './App.css';
 import Header from './components/Header'
+import Form from './components/Form';
 
 function App() {
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
   const [todos, setTodos] = useState([]);
-
-  const onTitleChange = (event) => setTitle(event.target.value);
-  const onBodyChange = (event) => setBody(event.target.value);
-
-  const onSubmitClick = (event) => {
-    event.preventDefault();
-    const newTodo = {
-      id: nanoid(),
-      title,
-      body,
-      isDone: false,
-    };
-    setTodos([...todos, newTodo]);
-    setTitle('');
-    setBody('');
-  };
 
   const onDeleteClick = (id) => {
     const deleteTodo = todos.filter(todo => todo.id !== id);
@@ -39,36 +21,7 @@ function App() {
   return (
     <div className="wrap">
       <Header />
-
-      {/* Form */}
-      <section className="form">
-        <form className="form__area">
-          <label htmlFor="form__title">
-            <strong>제목</strong>
-            <input 
-              type="text"
-              id="form__title"
-              value={title}
-              onChange={onTitleChange}
-            />
-          </label>
-          <label htmlFor="form__body">
-            <strong>내용</strong>
-            <input
-              type="text"
-              id="form__body"
-              value={body}
-              onChange={onBodyChange}
-            />
-          </label>
-          <button
-            className="form__button"
-            onClick={onSubmitClick}
-          >
-            <strong>추가하기</strong>
-          </button>
-        </form>
-      </section>
+      <Form todos={todos} setTodos={setTodos} />
 
       {/* List */}
       <section className="list">
---
+++ beginner/3_to-do-list/src/components/Form.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
+
+function Form({ todos, setTodos }) {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const onTitleChange = (event) => setTitle(event.target.value);
+  const onBodyChange = (event) => setBody(event.target.value);
+
+  const onSubmitClick = (event) => {
+    event.preventDefault();
+    const newTodo = {
+      id: nanoid(),
+      title,
+      body,
+      isDone: false,
+    };
+    setTodos([...todos, newTodo]);
+    setTitle('');
+    setBody('');
+  };
+
+  return (
+    <section className="form">
+      <form className="form__area">
+        <label htmlFor="form__title">
+          <strong>제목</strong>
+          <input 
+            type="text"
+            id="form__title"
+            value={title}
+            onChange={onTitleChange}
+          />
+        </label>
+        <label htmlFor="form__body">
+          <strong>내용</strong>
+          <input
+            type="text"
+            id="form__body"
+            value={body}
+            onChange={onBodyChange}
+          />
+        </label>
+        <button
+          className="form__button"
+          onClick={onSubmitClick}
+        >
+          <strong>추가하기</strong>
+        </button>
+      </form>
+    </section>
+  )
+}
+
+export default Form
