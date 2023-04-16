894d21089aa9a82be9967899425d12b71857428c
hansololiviakim <dev.hansolkim@gmail.com>
2023-04-17 03:45:36 +0900

    Feat: 등록, 조회, 상태값 변경 기능 추가

--- beginner/3_to-do-list/src/App.jsx
+++ beginner/3_to-do-list/src/App.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
 import './App.css';
 import Header from './components/Header'
 
 function App() {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const onTitleChange = (event) => setTitle(event.target.value);
+  const onBodyChange = (event) => setBody(event.target.value);
+  const onSubmitClick = (event) => {
+    event.preventDefault();
+    const newTodo = {
+      id: nanoid(),
+      title,
+      body,
+      isDone: false,
+    };
+    setTodos([...todos, newTodo]);
+    setTitle('');
+    setBody('');
+  };
+  const onStatusClick = (id) => {
+    const index = todos.findIndex(todo => todo.id === id);
+    let changeTodo = [...todos];
+    [...todos][index].isDone = !([...todos][index].isDone);
+    setTodos(changeTodo);
+  }
+
   return (
     <div className="wrap">
-      {/* Header */}
       <Header />
 
       {/* Form */}
@@ -16,6 +41,8 @@ function App() {
             <input 
               type="text"
               id="form__title"
+              value={title}
+              onChange={onTitleChange}
             />
           </label>
           <label htmlFor="form__body">
@@ -23,42 +50,81 @@ function App() {
             <input
               type="text"
               id="form__body"
+              value={body}
+              onChange={onBodyChange}
             />
           </label>
           <button
             className="form__button"
+            onClick={onSubmitClick}
           >
             <strong>추가하기</strong>
           </button>
         </form>
       </section>
 
-      {/* list */}
+      {/* List */}
       <section className="list">
         <div className="list__working">
-          <h1>Working</h1>
+          <h1>🔥Working🔥</h1>
           <div className="list__area">
-            <div className="todo">
-              <h1>111</h1>
-              <p>222</p>
-              <div className="btn">
-                <button
-                  className="btn__delete"
-                >
-                  삭제하기
-                </button>
-                <button
-                  className="btn__check"
-                >
-                  완료
-                </button>
-              </div>
-            </div>
+            {
+              todos.filter((todo) => {
+                return !todo.isDone
+              })
+              .map((todo) => {
+                return (
+                  <div className="todo" key={todo.id}>
+                    <h1>{todo.title}</h1>
+                    <p>{todo.body}</p>
+                    <div className="btn">
+                      <button
+                        className="btn__delete"
+                      >
+                        삭제하기
+                      </button>
+                      <button
+                        className="btn__check"
+                        onClick={() => onStatusClick(todo.id)}
+                      >
+                        {!todo.isDone ? '완료' : '취소'}
+                      </button>
+                    </div>
+                  </div>
+                )
+              })
+            }
           </div>
         </div>
         <div className="list__done">
-          <h1>Done</h1>
+          <h1>🎉Done🎉</h1>
           <div className="list__area">
+          {
+              todos.filter((todo) => {
+                return todo.isDone
+              })
+              .map((todo) => {
+                return (
+                  <div className="todo" key={todo.id}>
+                    <h1>{todo.title}</h1>
+                    <p>{todo.body}</p>
+                    <div className="btn">
+                      <button
+                        className="btn__delete"
+                      >
+                        삭제하기
+                      </button>
+                      <button
+                        className="btn__check"
+                        onClick={() => onStatusClick(todo.id)}
+                      >
+                        {!todo.isDone ? '완료' : '취소'}
+                      </button>
+                    </div>
+                  </div>
+                )
+              })
+            }
           </div>
         </div>
       </section>
