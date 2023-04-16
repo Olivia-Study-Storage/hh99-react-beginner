bcbde0b1d9d3c3b770821963130eed7cda4f87fc
hansololiviakim <dev.hansolkim@gmail.com>
2023-04-17 03:23:35 +0900

    Feat: 전체 UI 구현

--- beginner/3_to-do-list/src/App.jsx
+++ beginner/3_to-do-list/src/App.jsx
@@ -3,7 +3,68 @@ import './App.css';
 
 function App() {
   return (
-    <div></div>
+    <div className="wrap">
+      {/* Header */}
+      <section className="header">
+        <span className="header__title">My Todo List</span>
+        <span className="header__name">React</span>
+      </section>
+
+      {/* Form */}
+      <section className="form">
+        <form className="form__area">
+          <label htmlFor="form__title">
+            <strong>제목</strong>
+            <input 
+              type="text"
+              id="form__title"
+            />
+          </label>
+          <label htmlFor="form__body">
+            <strong>내용</strong>
+            <input
+              type="text"
+              id="form__body"
+            />
+          </label>
+          <button
+            className="form__button"
+          >
+            <strong>추가하기</strong>
+          </button>
+        </form>
+      </section>
+
+      {/* list */}
+      <section className="list">
+        <div className="list__working">
+          <h1>Working</h1>
+          <div className="list__area">
+            <div className="todo">
+              <h1>111</h1>
+              <p>222</p>
+              <div className="btn">
+                <button
+                  className="btn__delete"
+                >
+                  삭제하기
+                </button>
+                <button
+                  className="btn__check"
+                >
+                  완료
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="list__done">
+          <h1>Done</h1>
+          <div className="list__area">
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
 
