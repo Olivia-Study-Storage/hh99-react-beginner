27f06d3cd91ffa9373ac5a325f2a17e48ebbb41b
hansololiviakim <dev.hansolkim@gmail.com>
2023-04-18 12:38:27 +0900

    Note: Styled Comonents - Sass 개념 소개

--- intermediate/1_week-2/src/App.jsx
+++ intermediate/1_week-2/src/App.jsx
@@ -1,53 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
-import TestPage from './components/TestPage';
-import GlobalStyle from './components/GlobalStyle'
 
-// * GlobalStyles (전역 스타일링)
-// SC는 해당 컴포넌트 내에서만 사용 가능하다.
-// 프로젝트 전체를 아우르는 스타일을 적용할 때는 전역 스타일을 지정한다.
-
-const StContainer = styled.div`
-  display: flex;
-`;
-
-const StBox = styled.div`
-  width: 100px;
-  height: 100px;
-  border: 1px solid ${props => props.borderColor};
-  margin: 20px;
-`;
-
-const boxList = ['red', 'blue', 'green', 'pink'];
-
-// 색을 넣으면 이름을 반환
-const getBoxName = (color) => {
-  switch (color) {
-    case 'red':
-      return '빨간 박스';
-    case 'green':
-      return '초록 박스';
-    case 'blue':
-      return '파란 박스';
-    default:
-      return '검정 박스';
-  }
-}
+// * Sass(Syntactically Awesome Style Sheets)
+// 코드의 재사용성을 높이고 가독성 향상을 위해 사용하는 style sheet
+// 1. 변수를 사용할 수 있다.
+// 2. 중첩할 수 있다. (Nesting)
+// 3. 다른 style 파일을 import할 수 있다.
 
 function App() {
   return (
-  //   <StContainer>
-  //   {
-  //     boxList.map(box => <StBox borderColor={box}>{getBoxName(box)}</StBox>)
-  //   }
-  // </StContainer>
-    <>
-      <GlobalStyle />
-      <TestPage
-        title="제목입니다"
-        contents="내용입니다"
-      />
-    </>
+    <div>App</div>
   )
 }
 
