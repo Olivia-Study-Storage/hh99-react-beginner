9d3721a4265eeba3601c4c9391c1b44649011c50
hansololiviakim <dev.hansolkim@gmail.com>
2023-04-19 03:21:52 +0900

    Note: DOM 요소 접근 방법으로서의 useRef

--- intermediate/2_hooks/src/App.jsx
+++ intermediate/2_hooks/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from 'react';
+import { useEffect } from 'react';
 
 // * useRef란?
 // DOM 요소에 접근할 수 있도록 하는 react hook
@@ -13,45 +14,50 @@ import React, { useState, useRef } from 'react';
 // 2. DOM 요소 접근 방법으로서의 useRef
 //    - 렌더링되자마자 특정 input이 focusing되어야 한다면 useRef를 사용하면 된다.  
 
-const style = {
-  border: '1px solid black',
-  margin: '10px',
-  padding: '10px',
-}
 
 function App() {
-  const [count, setCount] = useState(0);
-  const countRef = useRef(0);
+  // TODO 퀴즈 -> id 필드가 10자리 입력되는 순간 비밀번호 필드로 포커스가 이동하도록 구현해보기
+  const [id, setId] = useState('');
 
-  // count의 숫자가 증가될 때마다 리렌더링이 일어난다.
-  const plusStateCountBtnHandler = () => setCount(count + 1);
-  // countRef.current의 값은 계속 증가되고 있지만 리렌더링은 일어나지 않는다.
-  const plusRefCountBtnHandler = () => {
-    countRef.current++;
-    console.log(countRef.current);
-  }
+  const idRef = useRef('');
+  const pwdRef = useRef('');
+
+  const idChangeHandler = (event) => setId(event.target.value);
+
+  // 화면이 렌더링될 때 어떤 작업을 하고 싶다면 useEffect 사용!
+  useEffect(() => {
+    idRef.current.focus();
+  }, []);
+
+  // ! id라는 state가 바뀔 때 해당 useEffect가 실행되어야 하므로 의존성 배열에 id를 넣는다.
+  // * onChange가 아니라 useEffect를 쓰는 이유?
+  // 리액트에서 state를 업데이트하는 방식이 '배치 업데이트'이다.
+  // 따라서 id라는 state를 set하는 방식이 10자리가 되었다고 바로 set하는게 아니라
+  // console.log로 확인해보면 11자리가 됐을 때 의도했던 대로 찍힐 것이다!
+  // 그래서 useEffect를 사용하는 것이다.
+  useEffect(() => {
+    if(id.length >= 10) {
+      pwdRef.current.focus();
+    }
+  }, [id])
 
   return (
     <>
-      <div style={style}>
-        state 영역입니다. {count}
-        <br />
-        <button
-          onClick={plusStateCountBtnHandler}
-        >
-          state 증가
-        </button>
+      <div>
+        아이디 :&nbsp;
+        <input 
+          type="text" 
+          value={id}
+          ref={idRef}
+          onChange={idChangeHandler}
+        />
       </div>
-
-      <div style={style}>
-        {/* countRef라는건 객체이기 때문에 그 안에 있는 초기값에 접근하려면 .current로 들어가야 한다. */}
-        ref 영역입니다. {countRef.current}
-        <br />
-        <button
-          onClick={plusRefCountBtnHandler}
-        >
-          ref 증가
-        </button>
+      <div>
+        비밀번호 :&nbsp;
+        <input 
+          type="password" 
+          ref={pwdRef} 
+        />
       </div>
     </>
   )
