e7e7ef53d9eaa87635846c41521e27f4fa15991f
hansololiviakim <dev.hansolkim@gmail.com>
2023-04-19 15:48:46 +0900

    Init: redux combineReducers, createStore 설정

--- intermediate/3_redux-counter/src/index.js
+++ intermediate/3_redux-counter/src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// store와 provider import
+import store from './redux/config/configStore';
+import { Provider } from 'react-redux';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
+  // App을 Provider로 감싸고, configStore에서 export default한 store를 넣어준다.
+  <Provider store={store}>
     <App />
-  // </React.StrictMode>
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
